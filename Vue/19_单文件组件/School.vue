84000136dc2b9d9de59a56b3bfdf1a7d69fbda93
tsubasatei <tsubasa_337283708@qq.com>
2021-11-03 11:00:40 +0800

    Vue基础学习

---
+++ Vue/19_单文件组件/School.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校名称：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {  
+            return {  
+                name: '尚硅谷',
+                address: '北京'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
